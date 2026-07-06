df010895251cf19f1f236c8accdc7051539f3868
s8ulkhumendra25-droid <s8ulkhumendra25@gmail.com>
2026-07-06 09:35:04 -0700

    Add Components index file

---
+++ app/src/main/kotlin/com/musicgo/app/ui/components/Components.kt
@@ -0,0 +1,12 @@
+package com.musicgo.app.ui.components
+
+// This file serves as an index for all reusable UI components
+
+// Export all components for easy importing
+export class SearchBar
+export class FilterChips
+export class SongListItem
+export class MiniPlayer
+export class CategoryPills
+export class ArtistCircleItem
+export class PlaylistCard
